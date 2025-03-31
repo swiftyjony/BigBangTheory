04a4230604fa4e8815b47fd9e0fee7d5abbba9fe
Jon Gonzalez <jon.gonzalez@mo2o.com>
2025-04-01 00:25:19 +0200

    Initial Commit

---
+++ BigBangTheory/BigBangTheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBangTheoryApp.swift
+//  BigBangTheory
+//
+//  Created by Jon Gonzalez on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BigBangTheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
